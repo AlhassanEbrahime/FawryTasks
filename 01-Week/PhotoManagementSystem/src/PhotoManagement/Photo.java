e1dfc7f9c9e48525248232ca92b5c2e352efa788
Hasan <hasan16121973@gmail.com>
2025-04-19 21:38:14 +0200

    add inverted index

--- 01-Week/PhotoManagementSystem/src/PhotoManagement/Photo.java
+++ 01-Week/PhotoManagementSystem/src/PhotoManagement/Photo.java
@@ -1,10 +1,50 @@
 package PhotoManagement;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 public class Photo {
     private String title;
     private String Location;
-    private String data;
+    private LocalDate data;
     private Set<String> tags;
+
+    public Photo(String title, String location, LocalDate data, Set<String> tags) {
+        this.title = title;
+        this.Location = location;
+        this.data = data;
+        this.tags = tags;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
 }
--- 01-Week/PhotoManagementSystem/src/PhotoManagement/PhotoManager.java
+++ 01-Week/PhotoManagementSystem/src/PhotoManagement/PhotoManager.java
@@ -5,4 +5,72 @@ import java.util.*;
 
 public class PhotoManager {
 
+    private Set<Photo> photos = new HashSet<>();
+    private Map<String , Set<Photo>> tagIndex = new HashMap<>();
+    private Map<String, Set<Photo>> locationIndex = new HashMap<>();
+    private Map<LocalDate,Set<Photo>> dateIndex = new HashMap<>();
+
+
+    public Photo addPhoto(String title , String location, LocalDate date , Set<String> tags){
+
+        Photo photo = new Photo(title, location, date, tags);
+
+        photos.add(photo);
+
+
+        for(String tag : tags){
+            tagIndex.computeIfAbsent(tag.toLowerCase(), k-> new HashSet<>()).add(photo);
+        }
+
+
+        locationIndex.computeIfAbsent(location.toLowerCase(), k-> new HashSet<>()).add(photo);
+
+
+        dateIndex.computeIfAbsent(date, k-> new HashSet<>()).add(photo);
+
+        return photo;
+
+    }
+
+
+    public boolean addTagsToPhoto(Photo photo, Set<String>newTags){
+
+        if(photos.contains(photo)){
+
+            for(String tag : newTags){
+
+                photo.getTags().add(tag.toLowerCase());
+
+                tagIndex.computeIfAbsent(tag.toLowerCase(), k-> new HashSet<>()).add(photo);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public Set<Photo> searchByTag(String tag){
+        if (tagIndex.containsKey(tag.toLowerCase())){
+            return tagIndex.get(tag);
+        }
+        return Collections.emptySet();
+    }
+
+
+    public Set<Photo> searchByLocation(String location){
+        if(locationIndex.containsKey(location)){
+            return locationIndex.get(location);
+        }
+        return Collections.emptySet();
+    }
+
+
+    public Set<Photo> SearchByDate(LocalDate date){
+        if(dateIndex.containsKey(date)){
+            return dateIndex.get(date);
+        }
+        return Collections.emptySet();
+    }
+
+    
+
 }
